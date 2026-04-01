10f02ae74b6a5fe67d039d9237a71f99fd011612
nicktanda <nick.havilah@tanda.co>
2026-04-01 21:12:01 +1000

    more mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,19 +40,10 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
-  # Use Gmail SMTP for sending emails
-  config.action_mailer.delivery_method = :smtp
+  # Email delivery via Resend (uses RESEND_API_KEY from initializer)
+  config.action_mailer.delivery_method = :resend
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "localhost",
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
