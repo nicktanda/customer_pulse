49749365142492f02876773cd8a279439d2457f3
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2026-05-10 23:12:01 +1000

    feat: add user-selectable accent colour picker with WCAG contrast validation

---
+++ apps/web/src/components/accent-color/index.ts
@@ -0,0 +1,3 @@
+export { AccentColorPicker, ACCENT_PALETTE, DEFAULT_ACCENT, passesWcagAA } from "./AccentColorPicker";
+export { AccentColorProvider, useAccentColor } from "./AccentColorProvider";
+export { AccentColorSettings } from "./AccentColorSettings";
