523876ac878f3c2fa3b1e00ffee412d47220f3db
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2026-05-10 23:18:57 +1000

    Revert "[xenoform.ai] Introduce a single user-selectable accent/highlight colour via a colour picker (#72)" (#73)
    This reverts commit cc629935631ed78d2b7e7e101f35200dbdf738c8.

--- apps/web/src/components/accent-color/index.ts
+++
@@ -1,3 +0,0 @@
-export { AccentColorPicker, ACCENT_PALETTE, DEFAULT_ACCENT, passesWcagAA } from "./AccentColorPicker";
-export { AccentColorProvider, useAccentColor } from "./AccentColorProvider";
-export { AccentColorSettings } from "./AccentColorSettings";
