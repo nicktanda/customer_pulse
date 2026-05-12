c1ed2e300f06ba4e0b199f61a932d16107024740
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2026-05-12 22:06:38 +1000

    fix: wire AccentColorProvider into layout, add settings page, fix CSS and code review issues

---
+++ apps/web/src/app/components/index.ts
@@ -0,0 +1,3 @@
+export { AccentColorProvider, useAccentColor, DEFAULT_ACCENT } from "./AccentColorProvider";
+export { AccentColorPicker } from "./AccentColorPicker";
+export { AccentColorSettings } from "./AccentColorSettings";
