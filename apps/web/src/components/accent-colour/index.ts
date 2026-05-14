e7a36c6a9d9cdc08f14f7c3381137c4af6d7e9ba
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2026-05-14 21:44:41 +1000

    feat: add accent colour picker to user settings/appearance page

---
+++ apps/web/src/components/accent-colour/index.ts
@@ -0,0 +1,5 @@
+export { AccentColourPicker } from "./AccentColourPicker";
+export { AccentColourProvider, useAccentColour } from "./AccentColourContext";
+export { AccentColourInit } from "./AccentColourInit";
+export { ACCENT_PALETTE, ACCENT_VALUES, DEFAULT_ACCENT } from "./accent-palette";
+export type { AccentColourKey } from "./accent-palette";
