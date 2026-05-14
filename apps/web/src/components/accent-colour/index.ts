03f0e6dfbea921938e57138dd72877a53bbdeed1
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2026-05-14 22:19:36 +1000

    feat: add accent colour picker to user preferences with global Bootstrap variable wiring

---
+++ apps/web/src/components/accent-colour/index.ts
@@ -0,0 +1,3 @@
+export { AccentColourProvider, useAccentColour, ACCENT_COLOURS, DEFAULT_ACCENT } from "./AccentColourProvider";
+export type { AccentColourId } from "./AccentColourProvider";
+export { AccentColourPicker } from "./AccentColourPicker";
