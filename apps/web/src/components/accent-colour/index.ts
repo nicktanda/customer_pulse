36fe471fbd8b849c9a1c6ec2ba4641da34dfa306
Nick Havilah <96028773+nicktanda@users.noreply.github.com>
2026-05-14 22:00:23 +1000

    feat: add accent colour picker to user settings with global CSS token wiring

---
+++ apps/web/src/components/accent-colour/index.ts
@@ -0,0 +1,5 @@
+export { AccentColourPicker } from "./AccentColourPicker";
+export { AccentColourProvider } from "./AccentColourProvider";
+export { ACCENT_COLOURS, DEFAULT_ACCENT_ID, getAccentColour } from "./accent-colours";
+export { applyAccentColour, getStoredAccentColour } from "./accent-colour-utils";
+export type { AccentColour } from "./accent-colours";
